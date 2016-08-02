6c9cdf7a5272a263d2475e49a40d51a3f81f6300
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-08-02 17:17:07 -0500

    item selected menu

--- ControlesBasicos/app/src/main/java/itokcenter/com/controlesbasicos/MainActivity.java
+++ ControlesBasicos/app/src/main/java/itokcenter/com/controlesbasicos/MainActivity.java
@@ -3,11 +3,15 @@ package itokcenter.com.controlesbasicos;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,25 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opc1:
+                Toast.makeText(getBaseContext(), "Opción 1 presionada", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.opc2:
+                Log.e(TAG, "onOptionsItemSelected: " + "opcion2");
+                return true;
+            case R.id.opc3:
+                Log.i(TAG, "onOptionsItemSelected: " + "opcion3");
+                return true;
+            case R.id.opc4:
+                Log.d(TAG, "onOptionsItemSelected: " + "Opcion 4");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
