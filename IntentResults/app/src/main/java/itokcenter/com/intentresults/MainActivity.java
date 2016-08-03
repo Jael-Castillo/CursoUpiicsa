a7e7b953c54b3751613608d5b1f4ab833aa6894d
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-08-03 17:58:13 -0500

    add camera intent

--- IntentResults/app/src/main/java/itokcenter/com/intentresults/MainActivity.java
+++ IntentResults/app/src/main/java/itokcenter/com/intentresults/MainActivity.java
@@ -1,14 +1,20 @@
 package itokcenter.com.intentresults;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnMail;
+    Button btnCamera;
+    ImageView imgCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnMail = (Button) findViewById(R.id.btnMail);
+        btnCamera = (Button) findViewById(R.id.btnCamara);
+        imgCamera = (ImageView) findViewById(R.id.imgCamera);
 
     }
 
@@ -37,5 +45,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intentCamera, 0);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            Log.e("Data Intent", data.toString());
+            Log.e("Data Extras", data.getExtras().toString());
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imgCamera.setImageBitmap(bitmap);
+        }
     }
 }
