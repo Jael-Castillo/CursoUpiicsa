2cd0aa0de46bc3cd47d33a1683c2fc0d4543ff10
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2016-08-04 08:47:03 -0500

    add permision sms

--- Permisos/app/src/main/java/itokcenter/com/permisos/MainActivity.java
+++ Permisos/app/src/main/java/itokcenter/com/permisos/MainActivity.java
@@ -1,13 +1,34 @@
 package itokcenter.com.permisos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnReadSMS;
+    TextView txtSMS;
+    String[] permisos = {Manifest.permission.READ_SMS};
+    int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReadSMS = (Button) findViewById(R.id.btnReadSms);
+        txtSMS = (TextView) findViewById(R.id.txtSms);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_SMS)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+            }
+        }
+
     }
 }
